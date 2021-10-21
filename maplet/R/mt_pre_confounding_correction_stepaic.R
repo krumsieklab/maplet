768b348afaa1549fd6a1bc823bd7e6df001c6dfe
richa <rib4003@med.cornell.edu>
2021-10-21 13:43:33 -0400

    updated - mt_pre_confounding_correction_stepaic to include columns names instead of numbers and more

--- maplet/R/mt_pre_confounding_correction_stepaic.R
+++ maplet/R/mt_pre_confounding_correction_stepaic.R
@@ -4,7 +4,8 @@
 #' in lieu of uncorrected features expressions.
 #'
 #' @param D \code{SummarizedExperiment} input.
-#' @param cols_to_correct Vector of column numbers from colData to correct for.
+#' @param cols_to_correct column names from colData to correct for.
+#' @param cols_to_exclude optional, column names from colData to exclude for correction.
 #' @param n_cores Number of cores to use in parallelization. Default: 1.
 #'
 #' @return assay: Corrected data.
@@ -18,54 +19,51 @@
 #' @author AS, RB
 #'
 #' @export
-mt_pre_confounding_correction_stepaic <- function(D, cols_to_correct, n_cores = 1) {
 
+mt_pre_confounding_correction_stepaic <- function(D, cols_to_correct, cols_to_exclude=NULL, n_cores = 1) {
+  
   # validate arguments
   stopifnot("SummarizedExperiment" %in% class(D))
-  stopifnot(is.numeric(cols_to_correct))
-
-  ####### there should be atleast one covariate information per sample
+  
+  # coldata for the SE
   Y <- D %>% colData() %>% data.frame()
-  # names of covariates
-  col_names <- names(Y)[cols_to_correct]
-  # which samples have atleast one non NA covariate information ?
-  non_na <- apply(Y [, cols_to_correct], 1, FUN=function(x)
-    length(which(is.na(x)))!=length(x)) %>% which()
-  # how many have no covariate information at all?
-  rem <- nrow(Y) - length(non_na)
-  if(rem > 0) mti_logwarning(sprintf("%d samples with no covariate info were removed!", rem))
-
-  #######  exclude the covariates with constant values for all patients
+  # all column names in colData
+  all_cols <- Y %>% names()
+  
+  # identify the columns with single level to be excluded
+  # exclude the covariates with constant values for all patients
   noNA <- function(x){x <- x[!is.na(x) & x!="NA" & x!=""];x}
-  cols_to_exclude <- NULL
-  for (cols in (cols_to_correct)) {
+  for (icol in (cols_to_correct)) {
     # just one factor represented?
-    if (D %>% colData() %>% as_tibble() %>% .[[cols]] %>% unique() %>% noNA() %>% length() <= 1) {
-      cols_to_exclude <- c(cols_to_exclude, cols)
-    }
-  }
+    if (Y %>% .[[icol]] %>% unique() %>% noNA() %>% length() <= 1) {
+      cols_to_exclude <- c(cols_to_exclude, icol)
+    } 
+  }   
   # remove the col number of covariates to exclude
   if(length(cols_to_exclude)>0){
-    cols_to_correct <- cols_to_correct[which(cols_to_correct%in%cols_to_exclude==F)]
+    cols_to_correct <- setdiff(cols_to_correct, cols_to_exclude)
     cols_to_exclude <- toString(cols_to_exclude)
-    mti_logwarning(sprintf("Covariates in column numbers %s with constant values were removed from correction!", cols_to_exclude))
+    warning(sprintf("Covariates in column numbers %s with constant values were removed from correction!", cols_to_exclude))
   }
-
   if(length(cols_to_correct)<1) stop("No covariates left to correct for!")
-
-
-  ####### subset the input based on missing covariate info
-  if(length(non_na) > 0) {
-    D <- D [, non_na]
-    X <- t(assay(D))
-    # bind covariate columns with features for modelling
-    model_data <- cbind.data.frame(X, colData(D)[, cols_to_correct])
-  } else stop("No samples with any covariate info!")
-
-  ####### loop over features
+  
+  # which samples have atleast one NA covariate information ?
+  samples_to_exclude <- apply(Y [, cols_to_correct], 1, FUN=function(x)
+    length(which(is.na(x)))>0) %>% which()
+  # subset the input based on missing covariate info
+  if((length(samples_to_exclude) > 0)) {
+    if(length(samples_to_exclude) < nrow(Y)){
+      D <- D [, -samples_to_exclude]  
+    } else stop("No samples with any covariate info!")
+  } 
+  
+  # loop over analytes
+  X <- t(assay(D))
+  # bind covariate columns with features for modelling
+  model_data <- cbind.data.frame(X, colData(D)[, cols_to_correct])
   outlist <- parallel::mclapply(1:ncol(X), FUN=function(i) {
     # all covariates and one feature
-    form <- paste(colnames(X)[i], "~", paste(col_names, collapse=" + "))
+    form <- paste(colnames(X)[i], "~", paste(cols_to_correct, collapse=" + "))
     # compute coefficients
     mod <- stats::lm(stats::as.formula(form), data = model_data)
     # stepwise selection of covariates that have an effect on feature
@@ -92,7 +90,7 @@ mt_pre_confounding_correction_stepaic <- function(D, cols_to_correct, n_cores = 
     res <- list(met_vec, list(colnames(X)[i], selected_covars, fit_pval, fit_rsq))
     return(res)
   }, mc.cores= n_cores)
-
+  
   ###### results gathering : bind outputs into assay for summarized experiment
   covar_adjusted <- do.call(rbind, lapply(outlist, function(x) unlist(x[[1]])))
   covars_log <- do.call(rbind, lapply(outlist, function(x) unlist(x[2])))
@@ -100,11 +98,11 @@ mt_pre_confounding_correction_stepaic <- function(D, cols_to_correct, n_cores = 
   colnames(covar_adjusted) <- colnames(assay(D))
   rownames(covar_adjusted)<- rownames(assay(D))
   assay(D) <- covar_adjusted
-
+  
   # add status information
-  funargs <- mti_funargs()
-  D %<>%
-    mti_generate_result(
+  funargs <- maplet:::mti_funargs()
+  D %<>% 
+    maplet:::mti_generate_result(
       funargs = funargs,
       logtxt = sprintf('Adjusted for covariate effects with stepAIC'),
       output = covars_log
