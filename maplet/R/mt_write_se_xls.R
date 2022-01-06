320330ea0739733613be773bd8cda4039f5da08c
KelseyChetnik <kelsey.chetnik@gmail.com>
2022-01-06 12:39:47 -0500

    Added keep_list_col arg to mt_write_se_xls
    Added keep_list_col argument to mt_write_se_xls. This allows the user to choose whether to keep or remove columns in rowData consisting of list values. If list columns are kept, the list values will be converted into strings seperated by '|'.

--- maplet/R/mt_write_se_xls.R
+++ maplet/R/mt_write_se_xls.R
@@ -3,39 +3,51 @@
 #' Exports the current \code{SummarizedExperiment} (not the metadata) to an Excel file.
 #'
 #' @param D \code{SummarizedExperiment} input.
-#' @param file Output filename to write to.
+#' @param file Output file name to write to.
+#' @param keep_list_col Whether to keep rowData columns containing list values. If TRUE, convert each list to a
+#'    string with values separated by "|". If FALSE, the column is removed. Default: FALSE.
 #'
 #' @return Does not change the \code{SummarizedExperiment} object.
 #'
 #' @examples
 #' \dontrun{%>% mt_write_se_xls(file = "out.xlsx") %>%}
 #'
-#' @author JK
+#' @author JK, KC
 #'
 #' @export
-mt_write_se_xls <- function(D, file) {
+mt_write_se_xls <- function(D, file, keep_list_col = FALSE) {
 
   # verify that input is a SummarizedExperiment
   stopifnot("SummarizedExperiment" %in% class(D))
   stopifnot(is.character(file))
 
-  # rowdata lists must be collapsed
+
+  # handle rowData list column(s)
   rd = rowData(D) %>% as.data.frame()
-  rd[] <- lapply(rd, function(x){if(is.list(x)){paste(x%>%unlist(),collapse="|")}else{x}})
+  list_cols <- lapply(seq_along(rd), function(x){if(is.list(rd[,x])){names(rd[x])}}) %>% unlist()
+  if(length(list_cols) > 0){
+    if(keep_list_col){
+      # collapse list values to strings
+      rd %<>% mutate_at(list_cols, collapse_list)
+    }else{
+      # remove list column(s)
+      rd %<>% dplyr::select(-all_of(list_cols))
+    }
+  }
 
   # write out
   wb <- openxlsx::createWorkbook()
   openxlsx::addWorksheet(wb,"assay")
   openxlsx::writeData(wb, "assay", assay(D), rowNames = T, colNames=T)
   openxlsx::addWorksheet(wb,"rowData")
-  openxlsx::writeData(wb, "rowData", rowData(D) %>% as.data.frame(), rowNames = T)
+  openxlsx::writeData(wb, "rowData", rd, rowNames = T)
   openxlsx::addWorksheet(wb,"colData")
   openxlsx::writeData(wb, "colData", colData(D) %>% as.data.frame())
   openxlsx::saveWorkbook (wb, file=file, overwrite=TRUE)
 
   # add status information
   funargs <- mti_funargs()
-  D %<>% 
+  D %<>%
     mti_generate_result(
       funargs = funargs,
       logtxt = sprintf("Data exported to Excel file '%s'", file)
@@ -45,4 +57,21 @@ mt_write_se_xls <- function(D, file) {
   # pass SummarizedExperiment back, so pipeline can keep running
   D
 
+}
+
+
+#' Collapse List Value into String
+#'
+#' Accepts a list value and returns a string with each element of the list separated by '|'.
+#'
+#' @param x A list.
+#'
+#' @returns A string of concatenated list values.
+#'
+#' @noRd
+collapse_list <- function(x){
+  x <- lapply(x, function(y){if(!is.null(y)){
+    paste0(y%>%unlist(),collapse = "|")
+  }else{y}})
+  x
 }
