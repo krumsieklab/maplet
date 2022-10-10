dcad7175b06684c526e13da4988b93df2db73313
KelseyChetnik <kelsey.chetnik@gmail.com>
2022-10-10 16:46:44 -0400

    bug fix - mt_clean_validate_se missing rownames
    Downstream issues as result of mt_clean_validate_se not checking for missing rownames. Now assigns numbers to rows if missing.

--- maplet/R/mt_clean_validate_se.R
+++ maplet/R/mt_clean_validate_se.R
@@ -16,35 +16,39 @@
 #'
 #' @export
 mt_clean_validate_se <- function(D){
-  
+
   # check rowData contains a column called "name"
   feat_col_names <- D %>% rowData() %>% colnames()
   if("name" %in% feat_col_names == F) stop("rowData must contain a column called 'name'.")
-  
+
   # check rowData column names are R Valid
   valid_feat_names <- feat_col_names %>% make.names()
   m <- match(valid_feat_names, feat_col_names) %>% is.na()
   if(any(m)) stop(glue::glue("The following rowData column names are not valid: {feat_col_names[m]}"))
-  
+
+  # add rownames if missing
+  #   missing rownames creates issues with functions such as stats_univ_lm
+  if(is.null(rownames(D))) rownames(D) <- seq(1, nrow(D))
+
   # check colData column names are R valid
   samp_col_names <- D %>% colData() %>% colnames()
   valid_samp_names <- samp_col_names %>% make.names()
   m <- match(valid_samp_names, samp_col_names) %>% is.na()
   if(any(m)) stop(glue::glue("The following colData column names are not valid: {samp_col_names[m]}"))
-  
+
   # check metadata empty and initialize entry
   if(metadata(D) %>% length() != 0) stop("SE object metadata must be empty.")
   metadata(D) <- list(sessionInfo=utils::sessionInfo())
-  
+
   # add status information
   funargs <- mti_funargs()
-  D %<>% 
+  D %<>%
     mti_generate_result(
       funargs = funargs,
       logtxt = "External SE object validated maplet-compatible."
     )
-  
+
   # return
   D
-  
+
 }
