a45a78b8989c115ed409e2ad9eee5b606c8119a8
KelseyChetnik <kelsey.chetnik@gmail.com>
2023-04-27 14:55:08 -0400

    New functions: mtm_res_get_plots_by_name and type
    Two new functions have been added: mtm_res_get_plots_by_name and mtm_res_get_plots_by_type. These allow users to easily access plot results either by a name associated with the plot (for example, via stat_name or stat_list) or by a substring within the function name (for example, 'volcano' returns all volcano plots).

--- maplet/R/mtm_access.R
+++ maplet/R/mtm_access.R
@@ -49,7 +49,7 @@ mtm_res_get_entries <- function(D,name_list) {
 #' @param fullstruct optional, output entire $output structure, not just the $table inside.
 #'
 #' @return $output$table dataframe
-#' 
+#'
 #' @export
 mtm_get_stat_by_name <- function(D, name, fullstruct=F){
   stopifnot("SummarizedExperiment" %in% class(D))
@@ -108,7 +108,7 @@ mtm_res_get_plots <- function(D,unlist=T){
 #' @param input List of plots or SummarizedExperiment
 #' @param dev Device to plot to (default: PDF)
 #' @param ... Further paramaters to be passed to dev() function.
-#' 
+#'
 #' @return None
 #'
 #' @export
@@ -125,129 +125,275 @@ mtm_plot_all_tofile <- function(input, dev=pdf, ...) {
 
 
 #' Extract all UUIDs for each function call
-#' 
+#'
 #' Extracts all of the UUIDs for each function call and returns a data frame with each function call and corresponding UUID.
-#' 
+#'
 #' @param D \code{SummarizedExperiment} input.
-#' 
+#'
 #' @return A data frame with three columns: (1) call_order, (2) function_calls, (3) UUIDs.
-#' 
+#'
 #' @export
 mtm_res_get_uuids <- function(D){
-  
+
   # validate arguments
   stopifnot("SummarizedExperiment" %in% class(D))
-  
+
   # extract function names and uuids
   fun_calls <- metadata(D)$results %>% names()
   calls <- sapply(fun_calls, function(x){x %>% strsplit("\\.") %>% unlist() %>% extract2(1)})
   uuids <- sapply(fun_calls, function(x){x %>% strsplit("\\.") %>% unlist() %>% extract2(2)})
-  
+
   # combine into data frame
   fun_uuid_df <- data.frame(call_order = 1:length(calls), function_calls = calls, UUIDs = uuids)
-  
+
   fun_uuid_df
 }
 
 
 #' Extract all assay pointers for each function call
-#' 
-#' Extracts all assay pointers for each function call and returns a data frame with each function call and corresponding 
+#'
+#' Extracts all assay pointers for each function call and returns a data frame with each function call and corresponding
 #' assay pointer. The assay pointer is an identifier used to determine which assay version was used with a particular function
 #' call. Will be NULL for all values if the global setting save_all_assays is FALSE.
-#'  
+#'
 #' @param D \code{SummarizedExperiment} input.
-#'  
+#'
 #' @return A data frame with two columns: (1) call_order, (2) function_calls, (3) assay_ptrs.
-#'   
-#' @export 
+#'
+#' @export
 mtm_res_get_ptrs <- function(D){
-  
+
   # validate arguments
   stopifnot("SummarizedExperiment" %in% class(D))
-  
+
   # extract function names
   res <- metadata(D)$results
   fun_calls <- res %>% names()
   calls <- sapply(fun_calls, function(x){x %>% strsplit("\\.") %>% unlist() %>% extract2(1)})
-  
+
   # extract assay pointers
   tmp_assay_ptrs <- sapply(1:length(res), function(i){res[[i]]$assay_ptr})
   # fix NULL values and unlist
   assay_ptrs <- sapply(tmp_assay_ptrs, function(x){ifelse(is.null(x), NA, x)})
-  
+
   # combine into data frame
   assay_ptr_df <- data.frame(call_order = 1:length(calls), function_calls = calls, assay_ptrs=assay_ptrs)
-  
+
   assay_ptr_df
-  
+
 }
 
 #' Return a specific assay version given a UUID or tag name
-#' 
+#'
 #' Returns an assay version associated with a specific function call identified by the UUID or a tag name.
-#' 
+#'
 #' @param D \code{SummarizedExperiment} input.
 #' @param uuid A UUID associated with a specific function call.
 #' @param tag_name Name of a tag given to a section of a maplet pipeline.
-#' 
+#'
 #' @return An assay data frame.
-#' 
+#'
 #' @export
 mtm_get_assay_by_id <- function(D, uuid, tag_name){
-  
+
   # validate arguments
   stopifnot("SummarizedExperiment" %in% class(D))
-  
+
   if(missing(uuid) & missing(tag_name)) stop("Must provide a value for either uuid or tag name.")
   if(!missing(uuid) & !missing(tag_name)) stop("Only one of these arguments can be passed at a time: uuid, tag_name.")
-  
+
   if(!missing(tag_name)){
     # check tag name exists
     tag_name_list <- maplet::mtm_res_get_entries(D, c("reporting", "tag")) %>% purrr::map("output") %>% unlist()
     tag_name_idx <- match(tag_name, tag_name_list)
     if(is.na(tag_name_idx)) stop(glue::glue("The tag \'{tag_name}\' was not found."))
-    
+
     # get function call
     fun_call <- names(tag_name_list[tag_name_idx])
-    
+
   }else{
     # check uuid exists
     uuid_df <- mtm_res_get_uuids(D)
     if(uuid %in% uuid_df$UUIDs == F) stop(glue::glue("Value for uuid \'{uuid}\' not found in metadata $results list."))
-    
+
     # get function call
     fun_call <- rownames(uuid_df[uuid_df$UUIDs==uuid,])
-    
+
   }
-  
+
   #extract assay version
   assay_ptr <- metadata(D)$results[[fun_call]]$assay_ptr
   assay <- metadata(D)$assays$assay_lst[[assay_ptr]]
-  
+
   assay
-  
+
 }
 
 #' Return a specific assay version given an assay pointer
-#' 
+#'
 #' Returns an assay version identified by a specific assay pointer.
-#' 
+#'
 #' @param D \code{SummarizedExperiment} input.
 #' @param ptr A string used to identify assay versions.
-#' 
+#'
 #' @return An assay data frame.
-#' 
+#'
 #' @export
 mtm_get_assay_by_ptr <- function(D, ptr){
-  
+
   # check assay pointer exists
   all_assay_ptrs <- metadata(D)$assays$assay_lst %>% names()
   if(ptr %in% all_assay_ptrs == F) stop(glue::glue("Value for ptr {ptr} not found in metadata $assays list."))
-  
+
   # extract assay version
   assay <- metadata(D)$assays$assay_lst[[ptr]]
-  
+
   assay
-  
+
+}
+
+#' Extract plots from entries in metadata by a 'name argument'
+#'
+#' Extracts plots from metadata()$results entries if they contain a 'naming argument' (see
+#' description of plot_name argument) that is equal to the user provided name.
+#'
+#' @param D \code{SummarizedExperiment} input.
+#' @param plot_name Any name associated with a plot as a value of a function argument. 'Naming
+#'    arguments' include stat_name, stat_list, ml_name, stat1, stat2, and title.
+#'
+#' @returns List of plots for the associated with the user-provided name.
+#'
+#' @examples
+#' \dontrun{
+#' # make a volcano plot
+#' D %>% mt_plots_volcano(stat_name = "Age super pw",
+#'                        x = statistic,
+#'                        feat_filter = p.adj < 0.05,
+#'                        colour = p.adj < 0.05)
+#'
+#' # get plot with 'name' "Age super pw"
+#' D %>% mtm_res_get_plots_by_name("Age super pw")
+#'}
+#'
+#' @author KC
+#' @export
+mtm_res_get_plots_by_name <- function(D, plot_name){
+
+  # possible arguments containing plot names
+  POSS_NAMES <- c("title", "stat_name", "stat1", "stat2", "ml_name", "stat_list")
+
+  # run checks and extract list of all plots
+  stopifnot("SummarizedExperiment" %in% class(D))
+  if(missing(plot_name)) stop("A value must be provided for argument 'plot_name'.")
+  if(! ("results" %in% names(metadata(D)))) stop("no results element found in D")
+
+  all_plots <- mtm_res_get_entries(D, "plots")
+  if(length(all_plots) == 0) stop("No plots element found in D.")
+
+  # extract arguments from those plot results that have 'possible name' arguments
+  arg_list <- all_plots %>% purrr::map("args") %>% purrr::map(names) %>%
+    sapply(function(x){length(intersect(POSS_NAMES, x)) > 0}) %>%
+    unlist() %>% all_plots[.] %>% purrr::map('args')
+
+  # find plot result that matches user provided name
+  res_name <- sapply(names(arg_list), function(x){
+    y <- unlist(arg_list[[x]])
+    pn <- y[which(names(y) %in% POSS_NAMES)]
+    if(length(pn)==0) return(NULL)
+    if(pn == plot_name) return(x)
+    NULL
+  }) %>% unlist() %>% unname()
+
+  # extract plot object (if present) and return
+  if(is.null(q)) return(list())
+
+  if(length(res_name)==1){
+    plot_res <- all_plots[[res_name]]
+    plot_type <- plot_res$fun
+    if("net" %in% plot_type == F) return(plot_res$output)
+    list(output=plot_res$output, output2=plot_res$output2)
+  }
+
+  if(length(res_name) > 1){
+    plot_list <- lapply(all_plots[res_name], function(x){
+      plot_type <- x$fun
+      if("net" %in% plot_type==F) return(x$output)
+      return(list(output=x$output, output2=x$output2))
+    })
+    plot_list
+  }
+
+}
+
+#' Extract plots from entries in metadata by a type
+#'
+#' Extracts plots from metadata()$results entries of functions that contain the user-provided
+#' substring (via the plot_type argument) in their name. Note the substring can contain any portion
+#' of the function name except the prefix 'mt_'. See examples.
+#'
+#' @param D \code{SummarizedExperiment} input.
+#' @param plot_type A substring contained within the name of the function(s) of interest.
+#'
+#' @returns List of plots for the plot types of interest.
+#'
+#' @examples
+#' \dontrun{
+#' # make a volcano and sample boxplots plot
+#' D %>% mt_plots_volcano(stat_name = "Age super pw",
+#'                         'x = statistic,
+#'                         'feat_filter = p.adj < 0.05,
+#'                         'colour = p.adj < 0.05) %>%
+#'   mt_plots_sample_boxplot(color=Diagnosis, title = "Original", plot_logged = T) %>%
+#'   mt_pre_batch_median(batch_col = "BOX.NUMBER") %>%
+#'   mt_plots_sample_boxplot(color=Diagnosis, title = "After batch correction", plot_logged = T)
+#'
+#' # get all volcano plots
+#' D %>% mtm_res_get_plots_by_type("volcano")
+#'
+#' # get all sample boxplots
+#' D %>% mtm_res_get_plots_by_type("sample_boxplot")
+#'
+#' # the following also returns all sample boxplots
+#' D %>% mtm_res_get_plots_by_type("mple_boxpl")
+#'
+#' # this doesn't work
+#' D %>%  mtm_res_get_plots_by_type("mt_plots_sample_boxplot")
+#'}
+#'
+#' @author KC
+#' @export
+mtm_res_get_plots_by_type <- function(D, plot_type){
+
+  # run checks and extract list of all plots
+  stopifnot("SummarizedExperiment" %in% class(D))
+  if(missing(plot_type)) stop("A value must be provided for argument 'plot_type'.")
+  if(! ("results" %in% names(metadata(D))))
+    stop("No results element found in D.")
+
+  all_plots <- maplet:::mtm_res_get_entries(D, "plots")
+  if(length(all_plots) == 0)
+    stop("No plots element found in D.")
+
+  # get result names of plots that contain value of plot_type as a substring
+  res_names <- all_plots %>% purrr::map('fun') %>%
+    sapply(function(x){paste0(x,collapse = '_')}) %>%
+    sapply(function(x){grepl(plot_type,x)}) %>% q[.] %>% names()
+
+  # extract plot objects (if present) and return
+  if(length(res_names) > 1){
+    plot_list <- lapply(all_plots[res_names], function(x){
+      if(grepl("net",plot_type)==F) return(x$output)
+      return(list(output=x$output, output2=x$output2))
+    })
+    return(plot_list)
+  }
+
+  if(length(res_names)==1){
+    x <- all_plots[[res_names]]
+    if(grepl("net",plot_type)==F) return(x$output)
+    return(list(output=x$output, output2=x$output2))
+  }
+
+  return(list())
+
 }
